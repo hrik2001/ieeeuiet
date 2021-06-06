f6f1332db02135cf623bd84fdb300ed3e45ca79c
rik61072@gmail.com <rik61072@gmail.com>
2021-06-06 16:29:08 +0530

    latest commit

--- pages/about.js
+++ pages/about.js
@@ -10,8 +10,41 @@ export default function About(props){
             <title>About | IEEE UIET</title>
         </Head>
         <Bar color="white" background="#0462A4"/>
-        <Card className="about_card">
-            <img src="IEEEUIET.png" style={{width:"50vw"}} />
+        <Card className="about_card flex flex-row flex-wrap">
+            <div className="about_card_banner mx-auto flex" >
+                <img src="IEEEUIET.png" className="my-auto" />
+            </div>
+            <div className="about_card_text mx-auto pt-3">
+                <h1 className="test mx-auto">IEEE UIET Student Branch, Chandigarh Subsection</h1>
+                <p>IEEE-UIET student branch is a part of Delhi Section of region R-10 SAC of IEEE. The members form a part of a global community that is dedicated to advancing technological innovation and excellence for the benefit of humanity.
+                IEEE UIET envisions a conducive environment in the campus which provides a plethora of opportunities for students to evolve in knowledge as well as to showcase their indigenous ideas through a number of amazing initiatives like ICAER, ADROIT, RAECS etc.
+                Our core purpose is to foster technological innovation and excellence for the benefit of humanity, to fulfill which, we ensure that our members embark upon a fruitful journey of learning through IEEE's highly cited publications, conferences, technology standards, and professional and educational activities.
+                </p>
+            </div>
+        </Card>
+        <Card className="about_card flex flex-row flex-wrap">
+            <div className="about_card_banner mx-auto flex" >
+                <img src="IEEEUIET.png" className="my-auto" />
+            </div>
+            <div className="about_card_text mx-auto pt-3">
+                <h1 className="test mx-auto">University Institute of Engineering & Technology , Panjab University</h1>
+                <p>IEEE-UIET student branch is a part of Delhi Section of region R-10 SAC of IEEE. The members form a part of a global community that is dedicated to advancing technological innovation and excellence for the benefit of humanity.
+                IEEE UIET envisions a conducive environment in the campus which provides a plethora of opportunities for students to evolve in knowledge as well as to showcase their indigenous ideas through a number of amazing initiatives like ICAER, ADROIT, RAECS etc.
+                Our core purpose is to foster technological innovation and excellence for the benefit of humanity, to fulfill which, we ensure that our members embark upon a fruitful journey of learning through IEEE's highly cited publications, conferences, technology standards, and professional and educational activities.
+                </p>
+            </div>
+        </Card>
+        <Card className="about_card flex flex-row flex-wrap">
+            <div className="about_card_banner mx-auto flex" >
+                <img src="IEEEUIET.png" className="my-auto" />
+            </div>
+            <div className="about_card_text mx-auto pt-3">
+                <h1 className="test mx-auto">IEEE UIET Student Branch, Chandigarh Subsection</h1>
+                <p>IEEE-UIET student branch is a part of Delhi Section of region R-10 SAC of IEEE. The members form a part of a global community that is dedicated to advancing technological innovation and excellence for the benefit of humanity.
+                IEEE UIET envisions a conducive environment in the campus which provides a plethora of opportunities for students to evolve in knowledge as well as to showcase their indigenous ideas through a number of amazing initiatives like ICAER, ADROIT, RAECS etc.
+                Our core purpose is to foster technological innovation and excellence for the benefit of humanity, to fulfill which, we ensure that our members embark upon a fruitful journey of learning through IEEE's highly cited publications, conferences, technology standards, and professional and educational activities.
+                </p>
+            </div>
         </Card>
 
         <Footer />
